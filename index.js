043b8311834d03877aec73a3bad433569a98e16b
wesley <wesleysilvadesousa@gmail.com>
2021-04-08 14:57:45 -0300

    feat: Set type game and change description

--- index.js
+++ index.js
@@ -3,11 +3,25 @@
     "use-strict";
 
     var app = (function appController() {
+        var $buttonsGame = $('[data-js="buttons-game"]').get();
+        var $gameInfo = $('p[data-js="info"]').get();
+        var allGames = []
+        var currentGame = []
         return {
             init: function init() {
                 this.gameInfo();
-                console.log("aqui")
+                this.initEvents();     
             },
+
+            initEvents: function initEvents() {
+                document.addEventListener('click',  function(e) {
+                    var dataset = e.target.dataset
+                    console.log(dataset.gameType)
+                    if (dataset.gameType) 
+                        return app.setGameType(dataset.gameType)
+                }, true)
+            },
+
             gameInfo: function gameInfo() {
                 var ajax = new XMLHttpRequest();
                 ajax.open("GET", "games.json", true);
@@ -15,20 +29,47 @@
                 ajax.addEventListener("readystatechange", this.getGameInfo, false);
                 
             },
+
             getGameInfo: function getGameInfo() {
                 if (!app.isReady.call(this)) return;
-                var data = JSON.parse(this.responseText);
-                var $buttonsGame = $('[data-js="buttons-game"]').get();
-                $buttonsGame.appendChild(app.createButtonGameInfo(data));
+                var data= JSON.parse(this.responseText);
+                app.loadGames(data.types)
+                app.createButtonGameInfo(data);
+                app.setGameType(allGames[0].type)
                 
             },
-            createButtonGameInfo: function createButtonGameInfo(data) {
-                data.map(function(item) {
-                    var $button = document.createElement("button")
-                    $button.textContent = item.type;
-                    return $button           
+
+            setGameType: function setGameType(type) {
+                currentGame = app.getCurrentGame(type)[0]
+                $gameInfo.textContent = currentGame.description
+            },
+
+            getCurrentGame: function getCurrentGame (type) {
+                return allGames.filter((item) => {
+                  return item.type === type
                 })
             },
+
+            loadGames: function loadGames(data) {
+                allGames = data.map(function (item, index) {
+                    item.type = item.type.split(' ').join('-')
+                    return item
+                  })
+            },
+
+            createButtonGameInfo: function createButtonGameInfo(data) {
+                    data.types.map(function(item) {
+                    var $button = document.createElement('button')
+                    $buttonsGame.appendChild($button)
+                    var buttonTextNode = document.createTextNode(item.type)
+                    $button.appendChild(buttonTextNode)
+                    $button.setAttribute('class', 'button choose-game')
+                    $button.style.border = 'solid ' + item.color
+                    $button.style.color = item.color  
+                    $button.setAttribute('data-game-type', item.type)         
+                })
+            },
+
             isReady: function isReady() {
                 return this.readyState === 4 && this.status === 200;
             },
@@ -36,4 +77,5 @@
     })();
 
     app.init();
+
 })(window.DOM);
